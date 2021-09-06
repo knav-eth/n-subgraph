bf39c1bec3c3bd3cb604ab522f4d47a5983793f8
Knav <me@knav.atlas>
2021-09-06 09:39:59 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# The n Project Subgraph
+
+Deployed subgraph can be found [here](https://thegraph.com/legacy-explorer/subgraph/knav-eth/the-n-project)
---
+++ src/mapping.ts
@@ -0,0 +1,39 @@
+import { NProject, Transfer } from "../generated/NProject/NProject"
+import { N } from "../generated/schema"
+
+export function handleTransfer(event: Transfer): void {
+  let tokenId = event.params.tokenId
+  let n = N.load(tokenId.toString())
+
+  if (n == null) {
+    n = new N(tokenId.toString())
+
+    let contract = NProject.bind(event.address)
+    n.name = "N #" + tokenId.toString()
+
+    n.first = contract.getFirst(tokenId).toI32()
+    n.second = contract.getSecond(tokenId).toI32()
+    n.third = contract.getThird(tokenId).toI32()
+    n.fourth = contract.getFourth(tokenId).toI32()
+    n.fifth = contract.getFifth(tokenId).toI32()
+    n.sixth = contract.getSixth(tokenId).toI32()
+    n.seventh = contract.getSeventh(tokenId).toI32()
+    n.eighth = contract.getEight(tokenId).toI32()
+
+    let numbers: Array<i32> = [
+      n.first,
+      n.second,
+      n.third,
+      n.fourth,
+      n.fifth,
+      n.sixth,
+      n.seventh,
+      n.eighth,
+    ]
+
+    n.numbers = numbers
+  }
+
+  n.owner = event.params.to
+  n.save()
+}
